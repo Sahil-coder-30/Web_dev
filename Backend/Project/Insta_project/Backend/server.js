c4eff344dc4e4aafa33d626edb32004e9296a416
Sahil <sahilsharma3043@gmail.com>
2026-03-08 16:31:41 +0530

    fix: update server to use PORT from environment variables and ensure consistent logging

--- Backend/Project/Insta_project/Backend/server.js
+++ Backend/Project/Insta_project/Backend/server.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const app = require('./src/app');
 const connectToDb = require('./src/config/db')
-
+const PORT = process.env.PORT || 3000;
 connectToDb();
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000..,");
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}..,`);
 })
